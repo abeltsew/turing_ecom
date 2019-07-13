e391ab80b8b51ce86fdab27eff98288442166c7d
abeltsew <abeltsew@gmail.com>
2019-07-13 05:43:54 +0300

    production db with string

--- config/prodDB.js
+++ config/prodDB.js
@@ -2,11 +2,16 @@
 
 const mysql = require('mysql')
 
-let connection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
-})
+let connection = mysql.createConnection(process.env.CLEARDB_JADE_URL)
 
 module.exports = connection
+
+
+// let connection = mysql.createConnection({
+//     host: process.env.DB_HOST,
+//     user: process.env.DB_USER,
+//     password: process.env.DB_PASSWORD,
+//     database: process.env.DB_NAME
+// })
+
+// module.exports = connection
